9b2d59516bbab710656831b016c3c459cde78ecc
Joel Nakabayashi <joelnakabayashi@new-host-3.home>
2026-01-07 10:21:18 -0800

    Album: hard lock stamp D

--- src/minisite/Album.jsx
+++ src/minisite/Album.jsx
@@ -16,7 +16,7 @@ import {
   postMasterSave,
 } from "./catalog/catalogCore.js";
 
-const ALBUM_BUILD_STAMP = "STAMP-ALBUM-LOCKS-VERIFY-2026-01-07-A";
+const ALBUM_BUILD_STAMP = "STAMP-ALBUM-HARD-LOCK-2026-01-07-D";
 
 function withTimeout(promise, ms, msg) {
   return Promise.race([
