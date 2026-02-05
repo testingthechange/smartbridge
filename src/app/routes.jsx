cd6fb41e49666b31d86c49ab30abbed38ea27bd5
Joel Nakabayashi <joelnakabayashi@new-host-3.home>
2026-02-04 22:18:43 -0800

    Fix build: remove missing minisite page imports

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -4,11 +4,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import MiniSiteLayout from "../minisite/MiniSiteLayout.jsx";
 import Catalog from "../minisite/Catalog.jsx";
-import Album from "../minisite/Album.jsx";
-import Songs from "../minisite/Songs.jsx";
-import Meta from "../minisite/Meta.jsx";
-import NFTMix from "../minisite/NFTMix.jsx";
-
 import AdminSend from "./AdminSend.jsx";
 
 export default function AppRoutes() {
@@ -23,15 +18,17 @@ export default function AppRoutes() {
       <Route path="/minisite/:projectId" element={<MiniSiteLayout />}>
         <Route index element={<Navigate to="catalog" replace />} />
         <Route path="catalog" element={<Catalog />} />
-        <Route path="album" element={<Album />} />
-        <Route path="songs" element={<Songs />} />
-        <Route path="meta" element={<Meta />} />
-        <Route path="nft-mix" element={<NFTMix />} />
+
+        {/* placeholders until these pages exist in this repo */}
+        <Route path="album" element={<Navigate to="../catalog" replace />} />
+        <Route path="songs" element={<Navigate to="../catalog" replace />} />
+        <Route path="meta" element={<Navigate to="../catalog" replace />} />
+        <Route path="nft-mix" element={<Navigate to="../catalog" replace />} />
+
         <Route path="*" element={<Navigate to="catalog" replace />} />
       </Route>
 
-      {/* Default: send unknown routes to minisite instead of admin */}
-      <Route path="/" element={<Navigate to="/minisite/000000/catalog" replace />} />
+      {/* Default */}
       <Route path="*" element={<Navigate to="/minisite/000000/catalog" replace />} />
     </Routes>
   );
