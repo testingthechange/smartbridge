bc5fc99580c39a3b946d1ae3ae694e08ebe9af42
Joel Nakabayashi <joelnakabayashi@new-host-3.home>
2026-02-05 09:09:28 -0800

    Fix routing: remove 000000 minisite redirects; default to /admin/send

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -2,22 +2,23 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import MiniSiteLayout from "../minisite/MiniSiteLayout.jsx";
 import AdminSend from "./AdminSend.jsx";
+import MiniSiteLayout from "../minisite/MiniSiteLayout.jsx";
 
 export default function AppRoutes() {
   return (
     <Routes>
+      {/* Public root */}
+      <Route path="/" element={<Navigate to="/admin/send" replace />} />
+
       {/* Admin */}
       <Route path="/admin/send" element={<AdminSend />} />
 
-      {/* Minisite: layout handles :page itself */}
-      <Route path="/minisite" element={<Navigate to="/minisite/000000/catalog" replace />} />
-      <Route path="/minisite/:projectId" element={<Navigate to="catalog" replace />} />
+      {/* Minisite (page param style) */}
       <Route path="/minisite/:projectId/:page" element={<MiniSiteLayout />} />
 
-      {/* Default */}
-      <Route path="*" element={<Navigate to="/minisite/000000/catalog" replace />} />
+      {/* Fallback */}
+      <Route path="*" element={<Navigate to="/admin/send" replace />} />
     </Routes>
   );
 }
