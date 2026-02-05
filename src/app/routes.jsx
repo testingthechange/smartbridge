f178a1962bff80ea19d148706977a166543d7c24
Joel Nakabayashi <joelnakabayashi@new-host-3.home>
2026-02-04 20:40:27 -0800

    Fix routes: remove missing Home import (or add Home page)

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,42 +1,22 @@
 // FILE: src/app/routes.jsx
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-
-import Home from "../pages/Home.jsx";          // or landing page
-import Player from "../pages/Player.jsx";
-import Account from "../pages/Account.jsx";
-
-import MiniSiteLayout from "../minisite/MiniSiteLayout.jsx";
-import Catalog from "../minisite/Catalog.jsx";
-import Album from "../minisite/Album.jsx";
-import Songs from "../minisite/Songs.jsx";
-import Meta from "../minisite/Meta.jsx";
-import NFTMix from "../minisite/NFTMix.jsx";
-
 import AdminSend from "./AdminSend.jsx";
+import MiniSiteLayout from "../minisite/MiniSiteLayout.jsx";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      {/* ---------- PUBLIC ---------- */}
-      <Route path="/" element={<Home />} />
-      <Route path="/player/:shareId" element={<Player />} />
-      <Route path="/account/:shareId" element={<Account />} />
+      {/* Public root */}
+      <Route path="/" element={<AdminSend />} />
 
-      {/* ---------- MINISITE ---------- */}
-      <Route path="/minisite/:projectId" element={<MiniSiteLayout />}>
-        <Route index element={<Catalog />} />
-        <Route path="catalog" element={<Catalog />} />
-        <Route path="album" element={<Album />} />
-        <Route path="songs" element={<Songs />} />
-        <Route path="meta" element={<Meta />} />
-        <Route path="nft-mix" element={<NFTMix />} />
-      </Route>
-
-      {/* ---------- ADMIN ---------- */}
+      {/* Admin explicit */}
       <Route path="/admin/send" element={<AdminSend />} />
 
-      {/* ---------- FALLBACK ---------- */}
+      {/* Minisite */}
+      <Route path="/minisite/:projectId/:page" element={<MiniSiteLayout />} />
+
+      {/* Fallback should NOT point to admin */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
